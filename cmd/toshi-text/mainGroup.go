82cbfb5b33ba138fcff53ccb3ac87e6a6bddbd59
diop <fodesdiop@gmail.com>
2019-02-05 11:00:22 -0800

    update main group

--- cmd/toshi-text/mainGroup.go
+++ cmd/toshi-text/mainGroup.go
@@ -8,11 +8,18 @@ import (
 
 func MainGroup(e *echo.Echo) {
 	e.GET("/home", handlers.RenderHome)
-	e.GET("/login", handlers.Login)
+	e.GET("/users/:id", handlers.GetUserById)
+	e.GET("/wallet/:data", handlers.GetUserByWalletAddress)
+	e.GET("/phones/:id", handlers.GetPhoneNumberForUser)
+	e.GET("/phones", handlers.CheckIfPhoneNumberExists)
+	e.GET("/exchange-rate", handlers.getExchangeRate)
+	e.GET("/users/:id", handlers.GetBalance)
+	e.GET("/users/:id", handlers.GetDepositAddress)
 	e.GET("/users/:data", handlers.GetUser)
 	e.GET("/wallets/:data", handlers.GetWalletAddress)
 
 	e.POST("/users", handlers.AddUser)
-	e.POST("/phones", handlers.AddPhone)
+	e.POST("/user:id", handlers.AddPhoneNumber)
 	e.POST("/wallets", handlers.CreateWallet)
+	e.POST("/user/:id", handlers.SellCrypto)
 }
